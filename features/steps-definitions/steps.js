aee1a866d29ca353c85c31ad15ab655ed407c65a
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-12-01 01:38:28 +0100

    Update steps.js

--- features/steps-definitions/steps.js
+++ features/steps-definitions/steps.js
@@ -29,3 +29,17 @@ Then('I see that at least {int} item appears on the results page', async (number
     const itemsList = await $('div.item-cells-wrap.border-cells.items-grid-view.four-cells.expulsion-one-cell');
     await expect(itemsList).toHaveChildren({ gte: number});
 });
+
+When('I open {string} tab', async (position) => {
+    const tab = await $('a#trendingBanner_720202.header2021-portal');
+    await tab.click();
+});
+
+Then('I click on the shop logo in the top left corner', async () => {
+    const logo = await $('a.header2021-logo-img');
+    await logo.click();
+});
+
+Then('I am on the home page again', async () => {
+    await expect(browser).toHaveUrl('https://www.newegg.com/');
+});
